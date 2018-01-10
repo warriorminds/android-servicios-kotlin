7940150283df47bed94e850e8c1a6e049347e5e1
Rodrigo Guerrero <rodrigo.x.guerero.-ND@disney.com>
2018-01-10 16:26:28 -0600

    Iniciando y terminando un servicio iniciado.

---
+++ app/src/main/java/com/warriorminds/fundamentosservicios/servicios/PrimerServicioIniciado.kt
@@ -0,0 +1,37 @@
+package com.warriorminds.fundamentosservicios.servicios
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import android.widget.Toast
+import com.warriorminds.fundamentosservicios.ActividadPrincipal
+
+/**
+ * Created by rodrigo.guerrero on 10/01/2018.
+ */
+class PrimerServicioIniciado : Service() {
+
+    companion object {
+        val TAG = this::class.java.simpleName
+    }
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val texto = intent?.getStringExtra(ActividadPrincipal.MENSAJE)
+        val apagarAutomaticamente = intent?.getBooleanExtra(ActividadPrincipal.APAGAR_AUTOMATICAMENTE, false)
+        Toast.makeText(this, texto, Toast.LENGTH_SHORT).show()
+
+        apagarAutomaticamente?.let {
+            stopSelf(startId)
+        }
+
+        return Service.START_NOT_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "PrimerServicioIniciado ha sido destruido")
+    }
+}
