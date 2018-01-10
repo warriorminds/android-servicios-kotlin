00170ad70a8e89865a44117fc2b4abd083cb7e79
Rodrigo Guerrero <rodrigo.x.guerero.-ND@disney.com>
2018-01-10 15:26:07 -0600

    Creacion del proyecto

---
+++ app/src/main/java/com/warriorminds/fundamentosservicios/ActividadPrincipal.kt
@@ -0,0 +1,12 @@
+package com.warriorminds.fundamentosservicios
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActividadPrincipal : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.actividad_principal)
+    }
+}
