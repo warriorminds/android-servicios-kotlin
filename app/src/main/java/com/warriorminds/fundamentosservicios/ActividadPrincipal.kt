16d35deef6ca1cc1dc9f039c3b0a4250768da76f
Rodrigo Guerrero <rodrigo.x.guerero.-ND@disney.com>
2018-01-10 16:49:45 -0600

    Ciclo de vida de un servicio iniciado.

--- app/src/main/java/com/warriorminds/fundamentosservicios/ActividadPrincipal.kt
+++ app/src/main/java/com/warriorminds/fundamentosservicios/ActividadPrincipal.kt
@@ -1,10 +1,12 @@
 package com.warriorminds.fundamentosservicios
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import com.warriorminds.fundamentosservicios.servicios.PrimerServicioIniciado
+import com.warriorminds.fundamentosservicios.servicios.ServicioIniciadoCicloVida
 import kotlinx.android.synthetic.main.actividad_principal.*
 
 class ActividadPrincipal : AppCompatActivity() {
@@ -27,6 +29,10 @@ class ActividadPrincipal : AppCompatActivity() {
         }
 
         cbDetenerServicioAutomaticamente.setOnCheckedChangeListener { _, isChecked -> btnDetenerPrimerServicio.isEnabled = !isChecked }
+
+        btnCicloVidaServicioIniciado.setOnClickListener {
+            iniciarServicioIniciadoCicloVida()
+        }
     }
 
     private fun detenerPrimerServicio() {
@@ -38,6 +44,11 @@ class ActividadPrincipal : AppCompatActivity() {
         val texto = if (TextUtils.isEmpty(mensajePrimerServicioIniciado.text.toString())) "El usuario no escribió ningún mensaje" else mensajePrimerServicioIniciado.text.toString()
         intent.putExtra(MENSAJE, texto)
         intent.putExtra(APAGAR_AUTOMATICAMENTE, cbDetenerServicioAutomaticamente.isChecked)
+        startService(intent)
+    }
+
+    private fun iniciarServicioIniciadoCicloVida() {
+        val intent = Intent(this, ServicioIniciadoCicloVida::class.java)
         startService(intent)
     }
 }
